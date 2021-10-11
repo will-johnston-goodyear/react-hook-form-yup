8319403f43118e844217ca543b16232475b2a8be
will-johnston-goodyear <will_johnston@goodyear.com>
2021-10-11 09:58:33 -0400

    Adds form and validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,76 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+type Inputs = {
+	email: string,
+	password: string,
 }
 
-export default App
+/* 
+	Yup Schema Instance 
+
+	Yup uses `factory methods` for generating new instances and properties for schema validation. 
+
+		e.g.: `yup.string().email.required()`
+	
+	This methods (string(), required()) allow for new Yup objects to be instantiated without using the `new` keyword.
+*/
+const schema = yup.object().shape({
+	email: yup.string().email().required(),
+	password: yup.string().min(8).max(32).required(),
+});
+
+const App = () => {
+
+	const {
+		/** Allows registering an element with the form and applying the appropriate validation rules */
+		register,
+		/** Will only receive form data if validation is successful */
+		handleSubmit,
+		/** `formState` returns multiple values from the form, including the `errors` object */
+		formState: { errors },
+		/** Clears the form / resets registered elements to default state */
+		reset
+	} = useForm<Inputs>({
+		/** the `resolver` property in the config object accepts either externally validated schema or user-defined validation */
+		resolver: yupResolver(schema),
+	});
+
+	const onSubmitHandler = (data : Inputs) => {
+		console.log({ data });
+		reset();
+	}
+
+  return (
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
+      <h2>Lets sign you in.</h2>
+      <br />
+
+			<input
+				// Spreads in props associated with `register` method 
+				{...register('email')}
+				placeholder="email"
+				type="email"
+				required
+			/>
+			{/* Conditionally displays error messages */}
+			<p>{ errors.email?.message }</p>
+      <br />
+
+			<input
+				{...register('password')}
+        placeholder="password"
+        type="password"
+        required
+      />
+			<p>{ errors.password?.message }</p>
+      <br />
+
+      <button type="submit">Sign in</button>
+    </form>
+  );
+};
+
+export default App;
